17935c192301794b33f449cc2650946c82a4d14f
JodiS34 <147281995+JodiS34@users.noreply.github.com>
2023-10-29 23:16:22 -0400

    Updating app.js
    Currently working on the structure.

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+}
+
 let apiKey = "7413b84e1e3f7a605f2df00a1bf1d6f2";
-let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid={API key}`;
+let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid={Apikey}&units=metric`;
+
+axios.get(apiURL).then(displayTemperature);
