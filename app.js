afe90f3a38b72a65a00f3d5ffa440959ed8f0259
JodiS34 <147281995+JodiS34@users.noreply.github.com>
2023-10-30 00:16:18 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
 }
 
 let apiKey = "7413b84e1e3f7a605f2df00a1bf1d6f2";
